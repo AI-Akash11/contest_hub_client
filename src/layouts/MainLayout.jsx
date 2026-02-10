2e71653c71066b20c50991bbd917af0ca4315690
Ali Imam Akash <akash1437aliimam@gmail.com>
2026-02-10 17:21:58 +0600

    added scroll logic for better ux

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router'
 import Navbar from '../components/Shared/Navbar/Navbar'
 import Footer from '../components/Shared/Footer/Footer'
+import ScrollToTop from '../components/utils/ScrollToTop'
 const MainLayout = () => {
   return (
     <div>
+      <ScrollToTop></ScrollToTop>
       <Navbar />
       <div className='pt-18 min-h-[calc(100vh-68px)]'>
         <Outlet />
